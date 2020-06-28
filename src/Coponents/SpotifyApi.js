936a375395038004e828bd13b6ede1948b35f751
Antonio Hadrović <hadrovic1@gmail.com>
2020-06-28 14:50:58 +0200

    Tried React-Spotify-Api, not good

--- src/Coponents/SpotifyApi.js
+++ src/Coponents/SpotifyApi.js
@@ -1,65 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MyLoader from './MyLoader';
 
 
 
-const API = 'https://api.spotify.com/v1/albums';
 
 
 
 export default class SpotifyApi extends React.Component {
-    
+
     constructor(props) {
         super(props);
      
         this.state = {
-          hits: [],
-          isLoading: false,
-          error: null,
+          token: '',
+          id:  ''
         };
-        console.log(this.state)
+        console.log( {...this.state})
       }
-      
-    
 
-      componentDidMount() {
 
-        this.setState({ isLoading: true })
-        fetch(API)
-        .then (response => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                throw new Error ('Error has happened')
-            }
-        })
-        //   .then(response => response.json()) initial code
-          .then(data => this.setState({ hits: data.hits, isLoading: false }))
-          .catch(error => this.setState( {error, isLoading: false} ));
-          
-      };
- 
-       render() {
-        const { hits, isLoading, error } = this.state;
-        if (error) {
-            return <p>{error.message}</p>
-        }
-        
-        if (isLoading) {
-            // return <p>Loading from isLoading: true</p>
-            return <MyLoader />
-        }
-        console.log(this.state)
+    render() {
         return (
-          <ul>
-            {hits.map(hit =>
-              <li key={hit.objectID}>
-                <a href={hit.url}>{hit.title}</a>
-                <p>Written by {hit.author}</p>
-              </li>
-            )}
-          </ul>
-        );
-      }
+            
+                <div>
+                    <MyLoader />
+                <p>React-spotify-api not good</p>
+                </div>
+            
+            
+        )
+    }
 }
-
